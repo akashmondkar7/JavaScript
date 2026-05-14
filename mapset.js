766b1a73887b712d7e89596a8bf671312d53abcd
Your Name <akashmondkar03@gmail.com>
2026-05-14 20:14:48 +0530

    .

---
+++ mapset.js
@@ -0,0 +1,17 @@
+// forEach
+
+// const arr =[10,20,30,90,97,87];
+
+// arr.forEach((number)=>{
+// sum+=number;
+// })
+
+// console.log(sum);
+
+//filter
+
+const arr =[10,20,30,90,97,87];
+
+
+const newArr=arr.filter ((number)=> number >30)
+console.log(newArr);

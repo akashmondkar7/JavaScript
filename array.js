4305db806d9dbe8d51d7bb65ae5f769a1c02e9f1
Your Name <akashmondkar03@gmail.com>
2026-05-14 11:55:01 +0530

    revision

--- array.js
+++ array.js
@@ -1,2 +1,39 @@
 let arr =["apple","banana","mango"]
 console.log(arr);
+
+
+
+let colors = ["Red", "Blue", "Green","white"]
+  console.log(colors[0])
+    console.log(colors[3])
+
+
+    let fruits = ["Apple", "Banana"]
+
+fruits.push("orange")
+console.log(fruits);
+
+let numbers = [10, 20, 30, 40]
+numbers.pop(2)
+console.log(numbers);
+
+
+let names = ["Akash", "Rahul", "Sam"]
+for( let i =0 ; i< names.length; i++){
+    console.log(names[i])
+}
+
+let cities = ["Mumbai", "Pune", "Delhi", "Goa"]
+
+console.log(cities.length);
+
+
+let nums = [10, 20, 30, 40]
+
+let sum=0;
+
+for(let i =0 ; i< nums.length; i==){
+    sum = sum + num[i];
+}
+
+console.log(sum);
